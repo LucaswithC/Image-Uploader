4cf58a3589f56675a3599dd10c8fe9cf34a80b82
LucaswithC <lucas.kiers@eclipso.at>
2021-10-28 00:50:39 +0200

    Node Installation

--- image-uploader/src/App.js
+++ image-uploader/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./ImageUpload.css";
+import React from "react";
+import imagePlaceholder from './images/placeholder.svg'
 
-function App() {
+class ImageUpload extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="container">
+        <div id="card">
+          <Upload />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ImageUpload;
+
+function Upload() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="upload-container">
+      <h3>Upload your image</h3>
+      <p>File sould be Jpeg, Png, ...</p>
+      <div id="image-uploader">
+        <img src={imagePlaceholder} alt="Upload Placeholder" width="30%" />
+      </div>
+      <h6>Or</h6>
+      <button>Choose a file</button>
     </div>
   );
 }
-
-export default App;
